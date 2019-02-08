f7b2c814aeff77a78d85bbf4ee7afab5f514b74e
Artur M. Wolff <artur.m.wolff@gmail.com>
2019-02-08 14:28:22 +0100

    Initial stable version

---
+++ sdfmt_test.go
@@ -0,0 +1,39 @@
+package sdfmt
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestStackdriverFormatter_Format(t *testing.T) {
+	log := logrus.New()
+	log.SetOutput(os.Stderr)
+	log.SetFormatter(&StackdriverFormatter{})
+
+	log.WithFields(logrus.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	log.WithFields(logrus.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
+
+	log.WithFields(logrus.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Fatal("The ice breaks!")
+
+	// A common pattern is to re-use fields between logging statements by re-using
+	// the logrus.Entry returned from WithFields()
+	contextLogger := log.WithFields(logrus.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
+}
