a5c1e254dd7872a770bc005caf02b1dd66ded3dd
Artur M. Wolff <artur.m.wolff@gmail.com>
2019-02-19 20:51:12 +0100

    Fix tests

--- sdfmt_test.go
+++ sdfmt_test.go
@@ -19,7 +19,7 @@ func TestStackdriverFormatter_Format(t *testing.T) {
 		"animal": "walrus",
 		"size":   10,
 	}).Info("A group of walrus emerges from the ocean")
-	assert.Equal(t, buf.String()[:123], `{"labels":{"animal":"walrus","size":"10"},"message":"A group of walrus emerges from the ocean","severity":300,"timestamp":"`)
+	assert.Equal(t, buf.String()[:123], `{"labels":{"animal":"walrus","size":"10"},"message":"A group of walrus emerges from the ocean","severity":200,"timestamp":"`)
 
 	buf.Reset()
 	log.WithFields(logrus.Fields{
